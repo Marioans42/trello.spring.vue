bd061c667e1352746859fb578c558b41ccbc0234
marioans <marioans42@gmail.com>
2020-02-27 14:51:14 +0300

    add back-end request mapping

---
+++ src/main/java/com/taskagile/web/pages/MainController.java
@@ -0,0 +1,17 @@
+package com.taskagile.web.pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * MainController
+ */
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"/", "/login"})
+    public String entry() {
+        return "index";
+    }
+    
+}
